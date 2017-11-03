e52a9717ea20f233b5410bf7540d50f4a3a69ffa
Tamás Bódis <sn72709n@freemail.hu>
2017-11-03 16:08:51 +0100

    Add files via upload

---
+++ hm2.bat
@@ -0,0 +1,3 @@
+@echo off
+
+"pvengine.exe" +W1600 +H1200 +A0.3 +FN +L"c:/windows/fonts" +L"C:/Program Files (x86)/EAGLE-5.6.0/ulp/eagle3d/ulp//../povray" +O"C:/Users/Tamás/Documents/eagle/hm2/hm2.png" /RENDER "C:/Users/Tamás/Documents/eagle/hm2/hm2.pov" 
